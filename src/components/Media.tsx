4e44d1745f696311bda364cfe55c2cc5da51e125
machao <frmachao@126.com>
2023-12-20 11:36:06 +0800

    Update Media.tsx

--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -2,32 +2,8 @@ import { Box } from "@mui/material";
 import Link from "next/link";
 
 export default function Media() {
-  const mediaList = [];
   return (
     <div className=" py-4">
-      <div className=" flex items-center justify-center gap-x-4 max-sm:flex-col">
-        {mediaList.map(({ title, linkText, link }) => {
-          return (
-            <div
-              key={title}
-              className=" flex items-center gap-2 text-xl"
-            >
-              <span>{title}:</span>
-              <Box
-                component={Link}
-                href={link}
-                className=" hover:underline"
-                sx={{
-                  color: "primary.main",
-                }}
-              >
-                {linkText}
-              </Box>
-            </div>
-          );
-        })}
-      </div>
-
       <div className=" text-center">
         打赏地址☕️: 0x4f2371B8BBC8201a6A41bd8D463613Bc65537dec
       </div>
